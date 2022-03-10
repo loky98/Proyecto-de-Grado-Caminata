6e86f53267751b70f3d6dba9446c59790529d4d2
Diego Camacho <khasfklsdgkk@gmail.com>
2022-03-09 19:25:13 -0500

    [UPDATE] modelo de caminata sencillo

--- CaminataSencilla.m
+++ CaminataSencilla.m
@@ -2,19 +2,38 @@ clc;
 close all;
 clear;
 
-leg_higher = 40;
-Leg_lower = 30;
-height = 60;
+LegLength1 = 12;
+LegLength2 = 10;
+Height = 18;
+step = 100;
 
-linea = [1,5; 3,2];
-plot (linea);
-%[a, b] = centroidLine(linea);
+AngleMin = -acos((Height-LegLength2)/(LegLength1));
+AngleMax = acos((Height)/(LegLength1+LegLength2));
 
-%plot (a, b,'o');
-grid ;
+RangeAngleLeg1 = linspace(AngleMin,AngleMax,step);
 
-function [x,y] = centroidLine(linea)
-    % Esta función sirve para Hallar el centroide de una linea 
-    x = (linea(1)+linea(3))/2;
-    y = (3+3)/2;
-    end
+x = zeros(step,3);
+y = zeros(step,3);
+
+for i=1:step
+    x(i,2) = LegLength1 * sin(RangeAngleLeg1(i));
+    y(i,2) = LegLength1 * cos(RangeAngleLeg1(i));
+    Angleleg2 = acos((Height-y(i,2))/(LegLength2));
+    x(i,3) = LegLength2 * sin(Angleleg2) + x(i,2);
+    y(i,3) = Height;
+end
+
+y = y *-1;
+
+try
+   while 1
+       for i=1:step
+           plot(x(i,1:3),y(i,1:3))
+           title("algo");
+           axis([-15,15,-22,1]);
+           grid;
+           pause(0.01);
+       end
+   end
+catch exception
+end
